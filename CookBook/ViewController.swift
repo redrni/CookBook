bb0da7fba2d1c300b65e907d4e9d1601bdddf7e2
redrni <karpovanati@mail.ru>
2022-10-15 15:56:06 +0300

    Initial Commit

---
+++ CookBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookBook
+//
+//  Created by Наталья Карпова on 15.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
